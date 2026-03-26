ab5053e9773a6eed2c1f5f15af439ddba1793ef3
luis garces <luisgarcestorres@gmail.com>
2026-03-26 14:23:37 -0500

    JULIO - Versión PWA (App Móvil) con Manifest y Service Worker

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'julio-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './voice.js',
+  './youtube.js',
+  './avatar.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
